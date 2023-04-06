bab65b53e2a284de47a4fca5acf6e81e99f6aa41
Noah <noikolomidze@gmail.com>
2023-04-06 15:42:54 +0400

    easy one again, just practicing

---
+++ exercises/exer1.ml
@@ -0,0 +1,5 @@
+(*Exercise 1: write a function named circle area
+   it will take in argument 'r' (radius) and returns
+   the area of the circle*)
+
+let circle_area (r: float) : float = 3.14 *. r *.r  
