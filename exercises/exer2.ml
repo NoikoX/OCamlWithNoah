acab9ced3537e739fd7f37fb3af82e03383c6a00
Noah <noikolomidze@gmail.com>
2023-04-06 15:54:05 +0400

    corrected easy one agian

--- exercises/exer2.ml
+++ exercises/exer2.ml
@@ -1,2 +1,6 @@
 (*Write a function named power that takes in a power 'n' and a float 'x' and returns x ^ n*)
-let rec power (x:float) (n:float) : float = x ** n
+let rec power (x:float) (n:float) : float = 
+  if n = 0. then 1.0 
+  else  x *. power x (n-.1.);;
+
+ 
